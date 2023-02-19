7dcbc20120411a462961e862141791badd698189
akabishau <aleksey.kabishau@icloud.com>
2023-02-19 13:08:35 -0500

    feat: configure main tab bar controller and app appearance

---
+++ GitHubFollowers/Sceens/FavoritesVC.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritesVC.swift
+//  GitHubFollowers
+//
+//  Created by Aleksey Kabishau on 2/19/23.
+//
+
+import UIKit
+
+class FavoritesVC: UIViewController {
+	
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		view.backgroundColor = .systemBackground
+	}
+}
---
+++ GitHubFollowers/Sceens/SearchVC.swift
@@ -0,0 +1,18 @@
+//
+//  SearchVC.swift
+//  GitHubFollowers
+//
+//  Created by Aleksey Kabishau on 2/19/23.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+	
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		view.backgroundColor = .systemBackground
+	}
+}
