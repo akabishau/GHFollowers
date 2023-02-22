0cfe05d672dc6196bf90f8370a4d2215ccb1f863
akabishau <aleksey.kabishau@icloud.com>
2023-02-22 10:03:10 -0500

    feat: show loading view during network call

--- GitHubFollowers/Sceens/Core/FollowerListVC.swift
+++ GitHubFollowers/Sceens/Core/FollowerListVC.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class FollowerListVC: UIViewController {
+class FollowerListVC: GFDataLoadingVC {
 	
 	var username: String!
 	
@@ -51,10 +51,12 @@ class FollowerListVC: UIViewController {
 	
 	
 	private func getFollowers(username: String, page: Int) {
+		
+		showLoadingView()
 		NetworkManager.shared.getFollowers(for: username, page: page) { [weak self] result in
 			
 			guard let self = self else { return }
-			
+			self.dismissLoadingView()
 			switch result {
 				case .success(let followers):
 					if followers.count < 100 { self.hasMoreFollowers = false }
